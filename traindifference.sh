4745311d149b47f645ad1c0b73daff90ecd19f40
Your Name <youremail@domain.com>
2019-06-28 00:35:32 +0200

    Parameterize training script

--- traindifference.sh
+++ traindifference.sh
@@ -1,2 +1,2 @@
 rm -r ./output
-allennlp train --include-package xnym_embeddings.xnym_embeddings --include-package attention_please_tagger.attention_please_tagger ./difference.config -s ./output/
+allennlp train --include-package xnym_embeddings.xnym_embeddings --include-package attention_please_tagger.attention_please_tagger $1 -s ./output/
