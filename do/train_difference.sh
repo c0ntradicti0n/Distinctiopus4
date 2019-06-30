ad747023d7a9e737abb6176aff80cccd7715d756
Your Name <youremail@domain.com>
2019-06-30 18:42:07 +0200

    own output dir for experiment

--- do/train_difference.sh
+++ do/train_difference.sh
@@ -1,2 +1,2 @@
-rm -r ./output
-allennlp train --include-package xnym_embeddings.xnym_embeddings --include-package attention_please_tagger.attention_please_tagger $1 -s ./output/
+rm -r ./output/$1
+allennlp train --include-package xnym_embeddings.xnym_embeddings --include-package attention_please_tagger.attention_please_tagger $1 -s ./output/$1/
