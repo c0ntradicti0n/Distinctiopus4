3ceef20f76bfda2e725624831c556db33c4ee5b9
Your Name <youremail@domain.com>
2019-07-10 20:15:40 +0200

    Add setup to install with git from github

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+from pip._internal.req import parse_requirements
+from setuptools import setup, find_packages
+
+print (os.listdir("./"))
+
+requirements = parse_requirements('./requirements.txt', session='hack')
+requirements = [str(ir.req) for ir in requirements]
+print (requirements)
+setup(
+    name='Distinctipus4',
+    packages=find_packages(),
+    url='https://github.com/c0ntradicti0n/Distinctiopus4.git',
+    description='This is NLP software to mine propositions of differences (of course in texts).',
+    long_description=open('./README.md').read(),
+    install_requires= requirements,
+    dependency_links = [
+     "git+git://github.com/c0ntradicti0n/Distinctiopus4/tarball/master#egg=Distinctiopus4",
+    ],
+    include_package_data=True,
+)
