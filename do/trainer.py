7320f8cf908dbbd42300fbf6c35be65ca2092d74
SW <dagedieb@gmail.com>
2019-07-22 19:48:06 +0200

    add a __init__.py to everything

--- do/trainer.py
+++ do/trainer.py
@@ -7,7 +7,7 @@ from allennlp.models import load_archive
 import xnym_embeddings.xnym_embeddings
 import attention_please_tagger.attention_please_tagger
 import spacy_embedder.spacy_embedder
-params = Params.from_file('experiment_configs/difference_stacked_birectional_lstm3_without_elmo.config')
+params = Params.from_file('experiment_configs/difference_stacked_birectional_lstm5_spacy_elmo_nyms.config')
 serialization_dir = tempfile.mkdtemp()
 model = train_model(params, serialization_dir)
 
