70928166dc9783304a3b67a4009af1d9f9f0ecdf
SW <dagedieb@gmail.com>
2019-09-28 19:15:09 +0200

    straight, konvex, konkav nn

--- do/trainer.py
+++ do/trainer.py
@@ -7,7 +7,7 @@ from allennlp.models import load_archive
 import xnym_embeddings.xnym_embeddings
 import attentivecrftagger.attentivecrftagger
 import spacy_embedder.spacy_embedder
-params = Params.from_file('experiment_configs/elmo_lstm3_feedforward4_crf.config')
+params = Params.from_file('experiment_configs/elmo_lstm3_feedforward4_crf_straight.config')
 serialization_dir = tempfile.mkdtemp()
 model = train_model(params, serialization_dir)
 
